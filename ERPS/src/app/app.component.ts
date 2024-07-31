49ad46b316a944ba51e9c836d05bc556fa98b0cc
KrishneshvarCIT <121788987+KrishneshvarCIT@users.noreply.github.com>
2024-07-31 22:21:47 +0530

    Navbar modifications made

--- ERPS/src/app/app.component.ts
+++ ERPS/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ERPS';
 }
