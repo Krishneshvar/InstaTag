e60c83469ee8d3b6af18ac438e11082a7a526d05
Krishneshvar <krishjn07@gmail.com>
2024-08-08 12:51:38 +0530

    Some style and alignment changes with logo update

---
+++ ERPS/src/app/auth-form/auth-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-form.component.html',
+  styleUrl: './auth-form.component.css'
+})
+export class AuthFormComponent {
+
+}
