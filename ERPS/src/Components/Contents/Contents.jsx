ef1c09127073b55817efbacc62fc3c9a6fb8c18f
Krishneshvar <krishjn07@gmail.com>
2024-09-13 16:38:20 +0530

    Home page changes and car image set to bg of first div

--- ERPS/src/Components/Contents/Contents.jsx
+++ ERPS/src/Components/Contents/Contents.jsx
@@ -6,76 +6,83 @@ function Contents() {
     <>
       <main>
         <Navbar /> 
-        <div class="one d-flex flex-column justify-content-center align-items-center text-secondary px-4 py-5 text-center rounded-3 border shadow-lg">
-          <img class="car-img" src="/Designer.jpeg" alt="A car on a highway" />
-          <div class="img-overlay fade-in-text">
+        <div className="one d-flex flex-column justify-content-center align-items-center text-secondary px-4 py-5 text-center rounded-3 border shadow-lg">
+          {/* <img class="car-img" src="/Designer.jpeg" alt="A car on a highway" /> */}
+          <div className="img-overlay fade-in-text">
             <h1>InstaTag</h1>
             <h2>Efficiency on the go...</h2>
           </div>
         </div>
 
-        <div class="two d-flex flex-column justify-content-center align-items-center text-secondary px-4 py-5 text-center rounded-3 border shadow-lg">
-          <h1 class="display-4 fw-bold text-body-emphasis">Centered screenshot</h1>
-          <div class="col-lg-6 mx-auto">
-            <div class="card"></div>
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-              <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
-              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        <div className="two d-flex flex-column justify-content-center align-items-center text-secondary px-4 py-5 text-center rounded-3 border shadow-lg">
+          <h1 className="display-4 fw-bold text-body-emphasis">What we provide</h1>
+          <div className="d-flex col-lg-6 mx-auto justify-content-between align-items-center cards-container">
+            <div className="card">
+              <img src="/fast-icon.png" alt="Faster Icon" className='provides-icon' />
+              Faster
+            </div>
+            <div className="card">
+              <img src="/safe-icon.png" alt="Safer Icon" className='provides-icon' />
+              Safer
+            </div>
+            <div className="card">
+              <img src="/automated-icon.png" alt="Automated Icon" className='provides-icon' />
+              Automated
             </div>
           </div>
         </div>
 
-        <div class="b-example-divider"></div>
+        <div className="b-example-divider"></div>
 
-        <div class="container col-xxl-8 px-4 py-5">
-          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
+        <div className="container col-xxl-8 px-4 py-5">
+          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div className="col-10 col-sm-8 col-lg-6">
               .
             </div>
-            <div class="col-lg-6">
-              <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-              <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+            <div className="col-lg-6">
+              <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+              <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="b-example-divider"></div>
+        <div className="b-example-divider"></div>
 
-        <div class="container my-5">
-          <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-            <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-              <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Border hero with cropped image and shadows</h1>
-              <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+        <div className="container my-5">
+          <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+            <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+              <h1 className="display-4 fw-bold lh-1 text-body-emphasis">Border hero with cropped image and shadows</h1>
+              <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+                <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
               </div>
             </div>
-            <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
             </div>
           </div>
         </div>
 
-        <div class="b-example-divider"></div>
+        <div className="b-example-divider"></div>
 
-        <div class="bg-dark text-secondary px-4 py-5 text-center">
-          <div class="py-5">
-            <h1 class="display-5 fw-bold text-white">Dark color hero</h1>
-            <div class="col-lg-6 mx-auto">
-              <p class="fs-5 mb-4"></p>
-              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+        <div className="bg-dark text-secondary px-4 py-5 text-center">
+          <div className="py-5">
+            <h1 className="display-5 fw-bold text-white">Dark color hero</h1>
+            <div className="col-lg-6 mx-auto">
+              <p className="fs-5 mb-4"></p>
+              <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+                <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="b-example-divider mb-0"></div>
+        <div className="b-example-divider mb-0"></div>
       </main>
     </>
   );
