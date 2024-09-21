ef46cd01f308dd81d672319af4c808658eb43551
Krishneshvar <krishjn07@gmail.com>
2024-09-21 22:12:23 +0530

    env

--- ERPS/src/DBConnection.js
+++ ERPS/src/DBConnection.js
@@ -2,18 +2,18 @@ import express from 'express'
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = NODE_PORT;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "instatag",
-  password: "kichapostgresequel",
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 db.connect();
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-  });
+});
