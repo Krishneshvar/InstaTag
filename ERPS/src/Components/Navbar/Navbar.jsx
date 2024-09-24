0a5d84f05d87f8c6330cbda70782c7df208b7a66
Krishneshvar <krishjn07@gmail.com>
2024-09-24 12:45:42 +0530

    Routing to Home has been resolved.

--- ERPS/src/Components/Navbar/Navbar.jsx
+++ ERPS/src/Components/Navbar/Navbar.jsx
@@ -96,7 +96,7 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                            <Link className="nav-link items" to="/Contents">HOME</Link>
+                            <Link className="nav-link items" to="/">HOME</Link>
                             </li>
                             <li className="nav-item">
                              <Link className="nav-link items" to="/AboutUs">ABOUT US</Link>
