c0aaaca603b9ab63fc425e32c53f6b07497201fb
Krishneshvar <krishjn07@gmail.com>
2024-09-25 10:51:48 +0530

    Removed redundant libraries and Footer created.

--- ERPS/src/Components/Contents/Contents.jsx
+++ ERPS/src/Components/Contents/Contents.jsx
@@ -68,18 +68,17 @@ function Contents() {
 
         <div className="b-example-divider"></div>
 
-        {/* <div className="bg-dark text-secondary px-4 py-5 text-center">
+        <div className="bg-dark text-secondary px-4 py-5 text-center">
           <div className="py-5">
-            <h1 className="display-5 fw-bold text-white">Dark color hero</h1>
+            <h1 className="display-5 fw-bold text-white">Footer</h1>
             <div className="col-lg-6 mx-auto">
               <p className="fs-5 mb-4"></p>
               <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button>
+                Hello
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
 
         <div className="b-example-divider mb-0"></div>
       </main>
