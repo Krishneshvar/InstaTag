d53093a8814ebf99d8b41087bd6e7d73899b6949
ratheesh-kmr <ratheeshkmr2024@gmail.com>
2024-10-23 13:33:55 +0530

    Beautified Toll details

--- ERPS/src/Components/Navbar/Navbar.jsx
+++ ERPS/src/Components/Navbar/Navbar.jsx
@@ -41,11 +41,11 @@ function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about-us">About Us</Link>
+          <li className="nav-item">
+              <Link className="nav-link" to="/toll-details">Toll Details</Link> 
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/toll-details">Toll Details</Link> 
+              <Link className="nav-link" to="/about-us">About Us</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/contact">Contact</Link> 
--- ERPS/src/Components/TollDetails/TollDetails.jsx
+++ ERPS/src/Components/TollDetails/TollDetails.jsx
@@ -7,10 +7,10 @@ const TollDetails = () => {
     { vehicle_type: 'Car', toll_id: 1, booth_no: 'A1', toll_amt: 50 },
     { vehicle_type: 'Truck', toll_id: 2, booth_no: 'B1', toll_amt: 100 },
     { vehicle_type: 'Bus', toll_id: 3, booth_no: 'C1', toll_amt: 75 },
-    { vehicle_type: 'Van', toll_id: 5, booth_no: 'E1', toll_amt: 60 },
-    { vehicle_type: 'SUV', toll_id: 6, booth_no: 'F1', toll_amt: 80 },
-    { vehicle_type: 'Motorcycle', toll_id: 7, booth_no: 'G1', toll_amt: 25 },
-    { vehicle_type: 'Heavy_Truck', toll_id: 9, booth_no: 'I1', toll_amt: 150 },
+    { vehicle_type: 'Van', toll_id: 4, booth_no: 'E1', toll_amt: 60 },
+    { vehicle_type: 'SUV', toll_id: 5, booth_no: 'F1', toll_amt: 80 },
+    { vehicle_type: 'Motorcycle', toll_id: 6, booth_no: 'G1', toll_amt: 25 },
+    { vehicle_type: 'Heavy_Truck', toll_id: 7, booth_no: 'H1', toll_amt: 150 },
   ];
 
   return (
@@ -18,7 +18,7 @@ const TollDetails = () => {
       <h1>Toll Rates</h1>
       <div className="toll-list">
         {tollData.map((toll) => (
-          <div className="toll-item" key={toll.toll_id}>
+          <div className="glass" data-text={toll.vehicle_type} style={{ "--r": Math.floor(Math.random() * 30) }} key={toll.toll_id}>
             <img
               src={`/icons/${toll.vehicle_type.toLowerCase()}.png`}
               alt={`${toll.vehicle_type} icon`}
